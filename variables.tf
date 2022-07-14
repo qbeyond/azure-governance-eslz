208c331f4e852dbfc6610bc649e9a9bdc662e8d8
Theodor Wulff <theodor.wulff@qbeyond.de>
2022-07-14 15:07:58 +0200

    moved pim enabling into separate variable

--- variables.tf
+++ variables.tf
@@ -31,11 +31,20 @@ variable "group_assignments" {
   "<group_name>" = {
     service_principals = optional(list(string))    (list of service principals that should be added as members) 
     "<role>"           = list(string)              (<role> must be a role_definition_name or role_definition_id from azure, every element must be a scope: "mg:<mg_id>", "sub:<subscription_id>", "root" for Tenant Root Group or a full scope ID)
-    assignable_to_role = bool                      (Setting this value to true is a requirement for PIM, defaults to false)
 }
   ```
   DOC
   default = {}
+}
+
+variable "pim_enabled_groups" {
+  type = list(string)
+  description = <<-DOC
+  ```
+  pim_enabled_groups = ["groupA", "groupB"]     (sets the value of assignable_to_role to true)
+  ```
+  DOC
+  default = []
 }
 
 variable "management_group_policy_assignment_parameter_override" {
