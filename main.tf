39c1566d2680304f5bbb28cf3c298f8c53759e3c
QBY-MarkusMaring <106068259+QBY-MarkusMaring@users.noreply.github.com>
2023-12-21 11:38:26 +0100

    Maintain backwards compatibility for subscription associations

--- main.tf
+++ main.tf
@@ -85,7 +85,8 @@ module "governance_eslz" {
   
   disable_telemetry = true
   
-  custom_landing_zones    = local.caf_input
-  template_file_variables = var.template_file_variables
-  library_path            = var.library_path
+  custom_landing_zones            = local.caf_input
+  template_file_variables         = var.template_file_variables
+  strict_subscription_association = true
+  library_path                    = var.library_path
 }
